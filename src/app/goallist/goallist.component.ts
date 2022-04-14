36af6a89ab8b018c120e23fbb5e807e7d8799c7f
mercyakuku <mercyakuku24@gmail.com>
2022-04-14 22:10:31 +0300

    Add all

---
+++ src/app/goallist/goallist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-goallist',
+  templateUrl: './goallist.component.html',
+  styleUrls: ['./goallist.component.css']
+})
+export class GoallistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
